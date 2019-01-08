d996cfc58fc9990a754d1ec41ca9ca87caadbc8a
nitishgupta7 <nitish@sendinblue.com>
2019-01-08 22:31:08 +0530

    static listing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Employee from './Employee/Employee.js';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Nitish', age: 29 },
+      { name: 'Sachin', age: 28 },
+      { name: 'Gaurav', age: 20 }
+    ]
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>React app by nitish</h1>
+        <p>This is really working</p>
+        <button onClick={ () => this.switchNmaeHandler('Maximilaian!!')}>Click on</button>
+        <Employee 
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+          <Employee 
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}/>
+          <Employee 
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
       </div>
     );
   }
